5277f51bc1efa0f5696fbf9954b27ae388b2bd42
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-02-16 18:05:24 +0530

    caa

--- public/js/main_board.js
+++ public/js/main_board.js
@@ -20,7 +20,7 @@ $(document).ready(() => {
 
     $(".followerlist").on("click", (e) => {
         e.preventDefault();
-        
+
         $("#right_side_box").empty().html(`<p class="text-white m-5">Loading. . .</p>`);
 
         const followerusername = e.target.closest('.followerlist > div[data-username]').getAttribute('data-username');
@@ -37,9 +37,9 @@ $(document).ready(() => {
         })
     })
 
-    // search button functionality
+    // follwer search button functionality
     let search_btn = document.querySelector('#search_btn');
-    let searchbox = document.querySelector('.searchbox');
+    let searchbox = document.querySelector('#searchbox');
     let closesearch = document.querySelector('.closesearch');
 
     search_btn.addEventListener("click", () => {
@@ -47,13 +47,15 @@ $(document).ready(() => {
             searchbox.style.left = "0px";
             searchbox.style.transition = "all 0.2s";
         }
+        document.getElementById('followerSearchInput').focus();
     })
 
     closesearch.addEventListener("click", () => {
         if (searchbox.style.left == "0px") {
-            searchbox.style.left = "-100%";
+            searchbox.style.left = "-200%";
             searchbox.style.transition = "all 0.2s";
         }
+        document.getElementById('followerSearchInput').value = "";
     })
 
     // logout function
@@ -83,8 +85,16 @@ const checkfunc = async (username) => {
     window.location.reload();
 }
 
-// search user
-async function searchUser() {
+// global search user
+let globalSearchCont = document.querySelector('#globalSearchedUsername');
+let hideSearchContBtn = document.querySelector('#hideSearchCont');
+
+hideSearchContBtn.addEventListener('click', () => {
+    document.querySelector('#searchCont1').style.display = "none";
+})
+
+document.querySelector('#globalSearchBtn').addEventListener('click', async () => {
+
     let username = document.getElementById("searchInput").value.trim();
 
     if (username.length < 1) {
@@ -96,14 +106,54 @@ async function searchUser() {
         let response = await fetch(`/mainboard/search?username=${username}`);
         let data = await response.json();
 
+        globalSearchCont.innerHTML = "";
+        document.querySelector('#searchCont1').style.display = "block";
+
         if (response.ok) {
             data.forEach((user) => {
-                console.log(user.username);
+                if (user.username != loggedusername) {
+                    globalSearchCont.innerHTML += `<li class="flex items-center justify-between border-b border-gray-900 px-2 rounded-sm text-white">
+                        <div>
+                            <h2 class="text-md font-semibold tracking-wide">${user.username}</h2>
+                            <p class="text-gray-400 text-[11px] -mt-1">Tie-in with <span class="font-bold text-md text-green-600">${user.followed_user.length - 1}</span> users</p>
+                        </div>
+                        <div>
+                        <button class="py-2 px-3 rounded text-gray-400 hover:text-white" onclick="checkfunc('${user.username}');" title="Chat Now"><i class="fa-solid fa-comments"></i></button>
+                        <i class="fa-solid fa-share-from-square text-gray-400 hover:text-white" title="Share"></i>
+                        </div>
+                    </li>`
+                }
             })
         } else {
             console.log("user not available");
+            globalSearchCont.innerHTML = `<p class="text-gray-200">No username available!</p>`;
         }
     } catch (error) {
         console.error("Error fetching data:", error);
     }
+})
+
+// follower Search User
+const followerSearch = () => {
+    let followerSearchInput = document.querySelector('#followerSearchInput').value;
+    let followerSearchCont = document.querySelectorAll('.followerlist');
+
+    followerSearchCont.forEach((user) => {
+        let username = user.querySelector('div').getAttribute('data-username');
+
+            if(username && username.toLowerCase().includes(followerSearchInput)) {
+                user.style.display = "block";
+            } else {
+                user.style.display = "none";
+            }
+    })
 }
+
+// filter Button Functionality
+document.querySelector('#openFilterBtn').addEventListener('click', () => {
+    document.querySelector('#filterCont').style.display = "flex";
+})
+
+document.querySelector('#closeFilterBtn').addEventListener('click', () => {
+    document.querySelector('#filterCont').style.display = "none";
+})
--- src/controller/mainboard_controller.js
+++ src/controller/mainboard_controller.js
@@ -3,18 +3,18 @@ const mainboardquery = require('../query/mainboard_query');
 
 const controller1 = (req, res) => {
     datatable.query(mainboardquery.query2, [req.cookies.prattleuser], (error1, result1) => {
-        if(result1) {
-        datatable.query(mainboardquery.query1, [req.cookies.prattleuser], (error2, result2) => {
-            if (result2) {
-                res.render('main_board', { users: result2.rows, loggeduserfollowing: result1.rows[0].followed_user, username: req.cookies.prattleuser });
-            } else {
-                console.log(error2);
-                res.send(error2);
-            }
-        })
-    } else {
-        res.send("something gone wrong" + error1);
-    }
+        if (result1) {
+            datatable.query(mainboardquery.query1, [req.cookies.prattleuser], (error2, result2) => {
+                if (result2) {
+                    res.render('main_board', { users: result2.rows, loggeduserfollowing: result1.rows[0].followed_user, username: req.cookies.prattleuser });
+                } else {
+                    console.log(error2);
+                    res.send(error2);
+                }
+            })
+        } else {
+            res.send("something gone wrong" + error1);
+        }
     })
 }
 
@@ -28,17 +28,22 @@ const controller3 = (req, res) => {
     console.log("i am " + username);
 
     datatable.query(mainboardquery.query3, [username, req.cookies.prattleuser], (error, result) => {
-        console.log(result.rows[0].exists);
-
-        if(result.rows[0].exists) {
+        if (result.rows[0].exists) {
             res.redirect('/mainboard');
         } else {
             datatable.query(mainboardquery.query4, [username, req.cookies.prattleuser], (error, result) => {
-                if(error) {
+                if (error) {
                     console.log(error);
-                    res.send("something gone wrong to chat with them");
+                    res.send("something gone wrong to tie-in with");
                 } else {
-                    res.redirect('/mainboard');
+                    datatable.query(mainboardquery.query4, [req.cookies.prattleuser, username], (error, result) => {
+                        if (error) {
+                            console.log(error);
+                            res.send("something gone wrong to tie-in with");
+                        } else {
+                            res.redirect('/mainboard');
+                        }
+                    })
                 }
             })
         }
@@ -50,21 +55,21 @@ const controller4 = (req, res) => {
 
     if (!username) {
         return res.status(400).json({ error: "Username is required" });
-      }
+    }
 
-      try {
+    try {
         datatable.query(mainboardquery.query5, [`%${username}%`], (error, result) => {
 
-            if(result.rows > 0) {
-            res.json(result.rows);
-            console.log("result of " + username + " is : " + result.rows[0]);
+            if (result.rows.length > 0) {
+                res.json(result.rows);
+                console.log("result of " + username + " is : " + result.rows[0].followed_user);
             } else {
                 res.status(404).json("no user found");
             }
         })
-      } catch(error) {
+    } catch (error) {
         console.log("something gone wrong");
-      }
+    }
 }
 
 module.exports = { controller1, controller2, controller3, controller4 }
