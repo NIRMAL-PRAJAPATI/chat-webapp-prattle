2444c1039274d1a4fa923d32854fedd287c19daa
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-02-21 14:56:11 +0530

    `Refactor error handling and layout in main board and error pages`

--- src/middleware/checkcookie.js
+++ src/middleware/checkcookie.js
@@ -3,7 +3,7 @@ const checkcookie = (req, res, next) => {
     if(req.cookies.prattleuser) {
         next();
     } else {
-        res.status(404).render('error', { errorCode: 404, errorHeading: "Login/Register Required !", errorDescription: "Sorry, the page you are looking for is available only for loggedin users." });
+        res.status(404).render('error', { errorCode: 404, errorHeading: "Login/Register Required !", errorDescription: "Sorry, the page you are looking for is available after you login or register first." });
     }
 }
 
